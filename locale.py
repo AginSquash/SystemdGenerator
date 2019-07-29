265502fff73a0ad1c98ad9d959d4703c6cc9517f
AginSquash <agins.main@gmail.com>
2019-07-30 00:34:45 +0300

    Testing #1

---
+++ locale.py
@@ -0,0 +1,8 @@
+
+hello_msg = "Hello!/n It's SystemMD Generator!/nFor new autorun file type '1'."
+
+enter_new_name = "Enter name of you service-file (without '.service')"
+
+#Errors
+error_root = "You need root permissions to run this!"
+error_exist = "This file already exist!"
---
+++ main_generator.py
@@ -0,0 +1,31 @@
+import os
+import locale
+
+working_dir = "/etc/systemd/system/"
+
+def check_root():
+    try: 
+        os.rename('/etc/foo', '/etc/bar')
+    except IOError as e:
+        if (e[0] == errno.EPERM): 
+            print(locale.error_root)
+            sys.exit(1) 
+def main():
+    print(locale.hello_msg)
+    if input() == '1':
+
+    #handle = open("/etc/systemd/system/test.service", "w")
+    #handle.write("This is a test!")
+    #handle.close()
+
+def createNewService():
+    print(locale.enter_new_name)
+    file_name = str(input()) + ".service"
+    if not os.path.isfile(working_dir + file_name):
+        print(locale.error_exist)
+        break
+    print("Succeful!")
+
+if __name__ == "__main__":
+    check_root()
+    main()
